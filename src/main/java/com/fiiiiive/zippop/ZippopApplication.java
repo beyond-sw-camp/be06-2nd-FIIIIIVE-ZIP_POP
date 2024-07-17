b1705a6b3eaece45811f5fce17a49a95886d8949
mpqm <okqkrwhdtjd@gmail.com>
2024-07-17 09:33:52 +0900

    [Init] 프로젝트 초기 설정 (#2)

---
+++ src/main/java/com/fiiiiive/zippop/ZippopApplication.java
@@ -0,0 +1,13 @@
+package com.fiiiiive.zippop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZippopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZippopApplication.class, args);
+	}
+
+}
