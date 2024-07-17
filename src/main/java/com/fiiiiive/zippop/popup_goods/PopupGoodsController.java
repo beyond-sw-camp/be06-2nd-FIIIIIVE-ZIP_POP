32c0755f416ca57a0d29fab51604f9cd788ebe17
seongxun <rjqlrl30@naver.com>
2024-07-17 16:13:04 +0900

    [Feat]GOODS_001 완료, GOODS_002 완료

---
+++ src/main/java/com/fiiiiive/zippop/popup_goods/PopupGoodsController.java
@@ -0,0 +1,52 @@
+package org.fiiiiive.zippop.popup_goods;
+
+
+import org.fiiiiive.zippop.popup_goods.req.PopupGoodsReq;
+import org.fiiiiive.zippop.popup_goods.res.PopupGoodsRes;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/popup_goods")
+public class PopupGoodsController {
+    private final PopupGoodsService popupGoodsService;
+    public PopupGoodsController(PopupGoodsService popupGoodsService) {
+        this.popupGoodsService = popupGoodsService;
+    }
+
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody PopupGoodsReq popupGoodsReq) {
+        popupGoodsService.register(popupGoodsReq);
+        return ResponseEntity.ok("등록 성공");
+    }
+
+
+    @GetMapping("/search")
+    public ResponseEntity<Optional<List<PopupGoodsRes>>> search() {
+        Optional<List<PopupGoodsRes>> popupGoodsList = popupGoodsService.findAll();
+        return ResponseEntity.ok(popupGoodsList);
+    }
+
+    @GetMapping("/search_product_name")
+    public ResponseEntity<Optional<List<PopupGoodsRes>>> search_product_name(@RequestParam String product_name) {
+        Optional<List<PopupGoodsRes>> popupGoodsList = popupGoodsService.findByProductName(product_name);
+        return ResponseEntity.ok(popupGoodsList);
+    }
+
+    @GetMapping("/search_store_name")
+    public ResponseEntity<Optional<List<PopupGoods>>> search_store_name(@RequestParam String store_name) {
+        Optional<List<PopupGoods>> popupGoods = popupGoodsService.findByStoreName(store_name);
+        return ResponseEntity.ok(popupGoods);
+    }
+
+    @GetMapping("/search_product_price")
+    public ResponseEntity<Optional<List<PopupGoods>>> search_product_price(@RequestParam Integer product_price) {
+        Optional<List<PopupGoods>> popupGoodsList = popupGoodsService.findByProductPrice(product_price);
+        return ResponseEntity.ok(popupGoodsList);
+    }
+
+}
---
+++ src/main/java/com/fiiiiive/zippop/popup_goods/req/PopupGoodsReq.java
@@ -0,0 +1,15 @@
+package org.fiiiiive.zippop.popup_goods.req;
+
+import lombok.*;
+
+@Getter
+@Setter
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Builder
+public class PopupGoodsReq {
+    private String productName;
+    private Integer productPrice;
+    private Integer productAmount;
+    private String storeName;
+}
